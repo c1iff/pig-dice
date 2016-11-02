dff6a6631107d42ebcf45115d0fc7ea673abd84c
Chris Clifford and Daniel Munger <chris@>
2016-11-02 11:57:02 -0700

    add code to switch players after certian conditions are met

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-//
-var currentGame = new Game();
+// Buisness Logic
+
 
 function Player(username) {
   this.username = username;
@@ -14,19 +14,32 @@ function Game() {
 
 Player.prototype.rollDie = function() {
   var die = Math.floor(Math.random()*6 + 1);
-    if (die > 1) {
-      this.currentTurn += die;
-    } else {
-      this.currentTurn = 0;
-      currentGame.players.forEach(function(player){
-        if (player.isCurrentPlayer === true ) {
-          player.isCurrentPlayer = false;
-        } else if (player.isCurrentPlayer === false) {
-          player.isCurrentPlayer = true;
-        }
-      });
-    };
-    console.log(currentGame);
+  console.log(die);
+  if (die > 1) {
+    this.currentTurn += die;
+  } else {
+    this.currentTurn = 0;
+
+      nextPlayer();
+    //  currentGame.players.forEach(function(player){
+    //   if (player.isCurrentPlayer === true ) {
+    //     player.isCurrentPlayer = false;
+    //   } else if (player.isCurrentPlayer === false) {
+    //     player.isCurrentPlayer = true;
+    //   }
+     //});
+  };
+};
+var nextPlayer = function(){
+  var current = currentGame.getCurrentPlayer();
+  console.log(current);
+  currentGame.players[current].isCurrentPlayer = false;
+  if(current === currentGame.players.length - 1) {
+    currentGame.players[0].isCurrentPlayer = true;
+  } else{
+    current += 1;
+    currentGame.players[current].isCurrentPlayer = true;
+  }
 };
 
 Player.prototype.hold = function() {
@@ -39,12 +52,29 @@ Player.prototype.hold = function() {
       player.isCurrentPlayer = true;
     }
   });
-  console.log(currentGame);
 };
 
-// User Logic
-$(document).ready(function() {
+Game.prototype.getCurrentTurn = function() {
+  currentGame.players.forEach(function(player){
+    if (player.isCurrentPlayer === true ) {
+      return player.currentTurn;
+      //console.log(player.isCurrentPlayer)
+    }
+  });
+}
 
+Game.prototype.getCurrentPlayer = function() {
+  for (var i = 0; i < currentGame.players.length; i++) {
+    if (currentGame.players[i].isCurrentPlayer === true){
+      return i;
+    }
+  }
+}
+
+var currentGame = new Game();
+
+// User Interface Logic
+$(document).ready(function() {
   $("#player-profile").submit(function(event) {
     event.preventDefault();
     var playerName = $("#player-name").val();
@@ -55,15 +85,20 @@ $(document).ready(function() {
 
   $("#roll-die").submit(function(event) {
     event.preventDefault();
-    currentGame.players[0].rollDie();
-    $("#current-output").text(currentGame.players[0].currentTurn);
+    var current = currentGame.getCurrentPlayer();
+    currentGame.players[current].rollDie();
+    $("#current-output").text(currentGame.players[current].currentTurn);
+    //currentGame.getCurrentPlayer()
+    console.log(currentGame);
   });
 
   $("#hold").submit(function(event) {
     event.preventDefault();
-    currentGame.players[0].hold();
-    $("#score-output").text(currentGame.players[0].totalScore);
-    $("#current-output").text(currentGame.players[0].currentTurn);
+    var current = currentGame.getCurrentPlayer();
+    currentGame.players[current].hold();
+    $("#score-output").text(currentGame.players[current].totalScore);
+    $("#current-output").text(currentGame.players[current].currentTurn);
+    console.log(currentGame);
   });
 
 
